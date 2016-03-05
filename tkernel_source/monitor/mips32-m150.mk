1f3b2440bfb49642d85a8531a96cca3d3bf20a23
Du Huanpeng <u74147@gmail.com>
2016-03-06 00:18:49 +0800

    ports-mips32: import monitor source from tk2_m150
    most file place to hwdepend/mips32/mach-m150/, move them to proper
    place later.
    
    Signed-off-by: Du Huanpeng <u74147@gmail.com>

---
+++ tkernel_source/monitor/mips32-m150.mk
@@ -0,0 +1,22 @@
+SRC += hwdepend/mips32/boot/eitent.S
+SRC += hwdepend/mips32/boot/startup.c
+SRC += hwdepend/mips32/mach-m150/system.c
+SRC += hwdepend/mips32/mach-m150/sio.c
+SRC += hwdepend/mips32/mach-m150/command.c
+SRC += hwdepend/mips32/mach-m150/console.c
+SRC += hwdepend/mips32/mach-m150/string.c
+SRC += hwdepend/mips32/mach-m150/svc.c
+SRC += hwdepend/mips32/mach-m150/printf.c
+SRC += driver/sio/m150_serial.c
+
+
+OBJ := hwdepend/mips32/boot/eitent.o
+OBJ += hwdepend/mips32/boot/startup.o
+OBJ += hwdepend/mips32/mach-m150/system.o
+OBJ += hwdepend/mips32/mach-m150/sio.o
+OBJ += hwdepend/mips32/mach-m150/command.o
+OBJ += hwdepend/mips32/mach-m150/console.o
+OBJ += hwdepend/mips32/mach-m150/string.o
+OBJ += hwdepend/mips32/mach-m150/svc.o
+OBJ += hwdepend/mips32/mach-m150/printf.o
+OBJ += driver/sio/m150_serial.o
